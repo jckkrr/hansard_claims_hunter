112eb74c1bdb5539b2558199c0fe9506baaa68d6
Jack Kerr <69304112+jckkrr@users.noreply.github.com>
2023-02-08 22:34:37 +1100

    Add files via upload

--- hansard_claims_hunter_run.py
+++ hansard_claims_hunter_run.py
@@ -8,6 +8,21 @@ import streamlit as st
 import scrapingTools
 import hansard_claims_hunter_functions
 
+### 
+
+def getSoup(url):
+    
+    from requests import get
+    from bs4 import BeautifulSoup
+    import requests
+
+    user_agent='Mozilla/5.0 (Macintosh; Intel Mac OS X 10_12_1) AppleWebKit/602.2.14 (KHTML, like Gecko) Version/10.0.1 Safari/602.2.14'
+    headers = {'User-Agent': user_agent,'Accept':'text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,*/*;q=0.8'}
+    response = get(url, headers=headers)
+    soup = BeautifulSoup(response.text, 'lxml')
+
+    return soup
+
 ######
 
 def runScript():
@@ -34,7 +49,7 @@ st.write('# HANSARD CLAIMS HUNTER #####')
 
 st.write('Searches the latest Hansards for claims that can be analysed by fact checker.') 
 
-runScript()
+#runScript()
 
 
 st.write('')
